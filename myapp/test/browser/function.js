1a69ad3c4838b14c10afad9e75c07ba2e275e12e
Ashish Nautiyal <nautiyalashisha@gmail.com>
2016-05-24 16:15:13 +0530

    added moch and chai

---
+++ myapp/test/browser/function.js
@@ -0,0 +1,18 @@
+var assert = chai.assert;
+var should = chai.should();
+
+describe('Array', function() {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not  present', function () {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
+
+describe ('Sum' , function(){
+  it ("should return sum of two" , function () {
+      (2+3).should.equal(5)
+      //(5).should.be.exactly(5);
+  })
+})
